16af46e908eba11708312f391f85bf84e8382e91
ratulbmg <ratulbamangachi@gmail.com>
2022-09-14 17:51:19 +0530

    New Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+
+class OBJECT:
+    def __init__(self, b: int) -> None:
+        self.a = b
+
+    def odd_even(self) -> str:
+        if self.a % 2 == 0:
+            return f"{str(self.a)} = Even"
+        else:
+            return f"{str(self.a)} = Odd"
+
+    def __str__(self):
+        return f'The number is {self.a}'
+
+
+if __name__ == '__main__':
+    for i in range(1, int(input("enter number : ")) + 1):
+        obj = OBJECT(i)
+        print(obj.odd_even())
+        sleep(.01)
---
+++ temp.py
@@ -0,0 +1,30 @@
+def marge(lst):
+    if len(lst) > 1:
+        left_lst = lst[:len(lst) // 2]
+        right_lst = lst[len(lst) // 2:]
+
+        marge(left_lst)
+        marge(right_lst)
+
+        i = j = k = 0
+        while i < len(left_lst) and j < len(right_lst):
+            if left_lst[i] < right_lst[j]:
+                lst[k] = left_lst[i]
+                i += 1
+            else:
+                lst[k] = right_lst[j]
+                j += 1
+            k += 1
+        while i < len(left_lst):
+            lst[k] = left_lst[i]
+            i += 1
+            k += 1
+        while j < len(right_lst):
+            lst[k] = right_lst[j]
+            j += 1
+            k += 1
+
+
+arr = [1, 3, 0, 5, 8, 4, 5, 0, 7]
+marge(arr)
+print(arr)
